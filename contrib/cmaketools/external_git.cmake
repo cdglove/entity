b145c2da4642bccdd48fa0c1df30db03e112610f
Chris Glover <c.d.glover@gmail.com>
2014-10-25 10:56:25 -0400

    Automatic configuration of external git projects.

--- contrib/cmaketools/external_git.cmake
+++ contrib/cmaketools/external_git.cmake
@@ -9,7 +9,15 @@ if(NOT GIT_FOUND)
 endif()
 
 include (CMakeParseArguments)
-function(add_external_git_repo URL TAG TARGET_DIR)
+
+###############################################################################
+#
+# clone_external_git_repo
+# Clones a repo into a directory at configure time.  For more automated
+# project configuration, consider add_external_git_repo
+#
+###############################################################################
+function(clone_external_git_repo URL TAG TARGET_DIR)
   #set(single_value_args URL TAG TARGET_DIR) 
   #cmake_parse_arguments(t "${single_value_args}" ${ARGN})
 
@@ -36,4 +44,17 @@ function(add_external_git_repo URL TAG TARGET_DIR)
       message(STATUS "Failed to chechout ${TAG} on ${URL}")
     endif()
   endif()
+endfunction(clone_external_git_repo)
+
+###############################################################################
+#
+# add_external_git_repo
+# Adds a prepository into a prefix and automatically configurs it for use
+# using defalt locations. For more control, use clone_external_git_repo
+#
+###############################################################################
+function(add_external_git_repo URL TAG PREFIX)
+  clone_external_git_repo(${URL} ${TAG} "${PROJECT_SOURCE_DIR}/${PREFIX}")
+  add_subdirectory("${PROJECT_SOURCE_DIR}/${PREFIX}")
+  set(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "${PROJECT_BINARY_DIR}/${PREFIX}" )
 endfunction(add_external_git_repo)
