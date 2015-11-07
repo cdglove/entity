2d478cb004980ca1d0d9d06149d71a2c564d78eb
Chris Glover <c.d.glover@gmail.com>
2015-11-07 18:34:45 -0500

    Move headers to include/ directory.

---
+++ include/entity/config.hpp
@@ -0,0 +1,16 @@
+#ifndef ENTITY_CONFIG_H
+#define ENTITY_CONFIG_H
+
+#include <boost/config.hpp>
+#include <boost/config/warning_disable.hpp>
+
+namespace entity {
+
+const int VersionMajor = 1;
+const int VersionMinor = 0;
+const int VersionPatch = 0;
+char const* const VersionString = "1.0.0";
+
+}
+
+#endif // ENTITY_CONFIG_H
