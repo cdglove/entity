9b53c62a02a76e4c636b7cb0383a093f5b2f9e63
Chris Glover <c.d.glover@gmail.com>
2015-12-06 13:02:58 -0500

    Upgrade to the latest cmaketools.

--- contrib/cmaketools/external_git.cmake
+++ contrib/cmaketools/external_git.cmake
@@ -17,31 +17,31 @@ include (CMakeParseArguments)
 # project configuration, consider add_external_git_repo
 #
 ###############################################################################
-function(clone_external_git_repo URL TAG TARGET_DIR)
-  #set(single_value_args URL TAG TARGET_DIR) 
-  #cmake_parse_arguments(t "${single_value_args}" ${ARGN})
+function(clone_external_git_repo)
+  set(oneValueArgs URL TARGET_DIR TAG ALWAYS_UPDATE)
+  cmake_parse_arguments(clone_external "" "${oneValueArgs}" "" ${ARGN} )
 
-  if(NOT EXISTS "${TARGET_DIR}/.git")
-    message(STATUS "Cloning repo ${URL}")
+  if(NOT EXISTS "${clone_external_TARGET_DIR}/.git")
+    message(STATUS "Cloning repo ${clone_external_URL}")
     execute_process(
-      COMMAND "${GIT_EXECUTABLE}" clone ${URL} --branch ${TAG} ${TARGET_DIR}
+      COMMAND "${GIT_EXECUTABLE}" clone ${clone_external_URL} --branch ${clone_external_TAG} ${clone_external_TARGET_DIR}
       RESULT_VARIABLE error_code)
     if(error_code)
-      message(FATAL_ERROR "Failed to clone ${URL}")
+      message(FATAL_ERROR "Failed to clone ${clone_external_URL}")
     endif()
-  else()
-    message(STATUS "Updating repo ${URL}")
+  elseif(${clone_external_ALWAYS_UPDATE})
+    message(STATUS "Updating repo ${clone_external_URL}")
     execute_process(
       COMMAND "${GIT_EXECUTABLE}" pull
-      WORKING_DIRECTORY ${TARGET_DIR})
+      WORKING_DIRECTORY ${clone_external_TARGET_DIR})
     if(error_code)
-      message(STATUS "Failed to update ${URL}")
+      message(STATUS "Failed to update ${clone_external_URL}")
     endif()
     execute_process(
-      COMMAND "${GIT_EXECUTABLE}" checkout ${TAG} 
-      WORKING_DIRECTORY ${TARGET_DIR})
+      COMMAND "${GIT_EXECUTABLE}" checkout ${clone_external_TAG} 
+      WORKING_DIRECTORY ${clone_external_TARGET_DIR})
     if(error_code)
-      message(STATUS "Failed to chechout ${TAG} on ${URL}")
+      message(STATUS "Failed to chechout ${clone_external_TAG} on ${clone_external_URL}")
     endif()
   endif()
 endfunction(clone_external_git_repo)
@@ -53,9 +53,20 @@ endfunction(clone_external_git_repo)
 # using defalt locations. For more control, use clone_external_git_repo
 #
 ###############################################################################
-function(add_external_git_repo URL TAG PREFIX)
-  clone_external_git_repo(${URL} ${TAG} "${PROJECT_SOURCE_DIR}/${PREFIX}")
-  get_filename_component(full_path_source_dir "${PROJECT_SOURCE_DIR}/${PREFIX}" ABSOLUTE)
-  get_filename_component(full_path_bin_dir "${PROJECT_BINARY_DIR}/${PREFIX}" ABSOLUTE)
-  add_subdirectory(${full_path_source_dir} ${full_path_bin_dir})
+function(add_external_git_repo)
+  set(options ALWAYS_UPDATE ADD_SUBDIR)
+  set(oneValueArgs URL PREFIX TAG)
+  cmake_parse_arguments(external_git "${options}" "${oneValueArgs}" "" ${ARGN} )
+
+  clone_external_git_repo(
+    URL ${external_git_URL} 
+    TAG ${external_git_TAG} 
+    TARGET_DIR "${PROJECT_SOURCE_DIR}/${external_git_PREFIX}"
+    ALWAYS_UPDATE ${external_git_ALWAYS_UPDATE})
+
+  get_filename_component(full_path_source_dir "${PROJECT_SOURCE_DIR}/${external_git_PREFIX}" ABSOLUTE)
+  get_filename_component(full_path_bin_dir "${PROJECT_BINARY_DIR}/${external_git_PREFIX}" ABSOLUTE)
+  if(${external_git_ADD_SUBDIR})
+    add_subdirectory(${full_path_source_dir} ${full_path_bin_dir})
+  endif()
 endfunction(add_external_git_repo)
